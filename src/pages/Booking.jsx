ffc26284aef6c7f9df59e78e4b9d591e6e64c27e
gydalo <gylo_l@hotmail.com>
2026-03-02 17:46:28 +0100

    Booking page always scrolled to top when clicking on it

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,26 +1,33 @@
 import "../css/booking.css";
 import Header from "../components/Header";
+import { useEffect } from "react";
 
 export default function Booking() {
-  return (
-    <div>
-      <Header />
-      <section className="booking" aria-label="Booking">
-        <div className="booking__inner">
-          <h2 className="booking__title">Book din neste time her!</h2>
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, []);
 
-          <div className="booking__frameWrap">
-            <iframe
-              className="booking__frame"
-              src="https://bestill.timma.no/emblakosmetisk"
-              title="Timma booking – Embla Kosmetisk"
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              allow="fullscreen; payment"
-            />
+  return (
+    <main>
+      <div>
+        <Header />
+        <section className="booking" aria-label="Booking">
+          <div className="booking__inner">
+            <h2 className="booking__title">Book din neste time her!</h2>
+
+            <div className="booking__frameWrap">
+              <iframe
+                className="booking__frame"
+                src="https://bestill.timma.no/emblakosmetisk"
+                title="Timma booking – Embla Kosmetisk"
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                allow="fullscreen; payment"
+              />
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </main>
   );
 }
